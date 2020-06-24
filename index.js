8c0b0a75556a1ff0c2bb2347491f9a8e2db3761f
Louis SCHNEIDER <louis@schne.id>
2020-06-24 10:07:48 +0200

    add first configs

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'always',
+  proseWrap: 'never',
+  printWidth: 80
+}
